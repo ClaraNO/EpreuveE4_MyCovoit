a513267041e526e22c75305c629e9e2ac3bba3a1
claraN <claranoel144@gmail.com>
2021-04-01 10:25:52 +0200

    epreuve E4 CN

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFirestore } from '@angular/fire/firestore';
+//import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -8,10 +9,21 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+firebaseText: string;
+firebaseText1: string;
+firebaseText2: string;
+firebaseDateAller: Date;
+firebaseDateRetour: Date;
+firebaseHeureAller: Date;
+firebaseHeureRetour: Date;
+firebaseBouton: Boolean;
+
+
 
   constructor(
+    //private router: Router,
     public afDB: AngularFireDatabase,
-    public firestore: AngularFirestore
+    public firestore: AngularFirestore,
     ) {}
 
     add() {
@@ -20,5 +32,25 @@ export class Tab1Page {
       });
     }
 
+
+    addFirestore() {
+      this.firestore.collection('Items').add({
+      depart: this.firebaseText,
+      arrivee: this.firebaseText1,
+      etapes: this.firebaseText2,
+      dateAller: this.firebaseDateAller,
+      dateRetour: this.firebaseDateRetour,
+      heureAller:this.firebaseHeureAller,
+      heureRetour:this.firebaseHeureRetour,
+      bouton:this.firebaseBouton,
+
+      //var route = this.router,
+      //route.navigateByUrl('/tabs/tab2')
+      });
+    }	
+
+
 }
+
+
 
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, snapshotChanges } from '@angular/fire/database';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';//Récupérer les trajets
+import { Observable } from 'rxjs';//Récupérer les trajets
+import { Router } from '@angular/router';//Récupérer les trajets
+import { transform } from 'typescript';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
 
-  constructor() {}
+  items: Observable<any[]>;//Récupérer les trajets
 
+  listeTrajets = [];
+
+  constructor(public firestore: AngularFirestore,//Récupérer les trajets
+    public afDB: AngularFireDatabase,//Récupérer les trajets
+    public afSG: AngularFireStorage,//Récupérer les trajets
+  ) {
+    this.items= this.firestore.collection('Items').valueChanges();//Récupérer les trajets
+  }
+
+  getTrajet(){
+    var that = this;
+
+    this.listeTrajets = [];
+    this.items.subscribe(tra => {
+      tra.forEach(value1 => {
+        if (value1 ["depart"] != ""){
+          that.listeTrajets = value1
+        }
+    })
+    });
+  }
 }
