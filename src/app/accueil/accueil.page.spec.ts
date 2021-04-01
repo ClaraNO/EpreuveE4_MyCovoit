b4c6b4dff3234c5e92ed319dfd98c15dccdfbee5
claraN <claranoel144@gmail.com>
2021-04-01 13:57:29 +0200

    Erreur

--- src/app/accueil/accueil.page.spec.ts
+++ src/app/accueil/accueil.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
@@ -8,7 +8,7 @@ describe('AccueilPage', () => {
   let component: AccueilPage;
   let fixture: ComponentFixture<AccueilPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AccueilPage],
       imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
